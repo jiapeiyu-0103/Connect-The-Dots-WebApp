7d5f00017fbe4b1ddca59d43983507113ecd1be2
Eddy Dinh <eddydinh@Eddys-MacBook-Pro.local>
2021-06-07 12:20:17 -0700

    readd Message folder

---
+++ front-end/src/components/Message/DriftBottle.js
@@ -0,0 +1,16 @@
+function DriftBottle() {
+
+            return (
+                <div id="driftBotOuter">
+                    <div id="driftBotNav">
+                    <button> Send </button>
+                    <button> Collect </button>
+                    <button> My Bottles </button>
+                    </div>
+                </div>
+            );
+ 
+ 
+}
+
+export default DriftBottle;
--- front-end/src/components/Message/MessageMainView.js
+++ front-end/src/components/Message/MessageMainView.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import DriftBottle from './DriftBottle';
 import TreeHole from './TreeHole';
 import Chat from './Chat';
-import * as MessageStates from '../constants/MessageStates';
+import * as MessageStates from '../../constants/MessageStates';
 function MessageMainView(props) {
   switch(props.state) {
       case MessageStates.CHAT:
