355eb46cd812d2dffc3f2024ad3b31f1efa8ae66
Yang Liu <younglll2015@gmail.com>
2021-06-26 14:08:33 +0800

    new commit for merge

--- front-end/src/components/Account/LoginForm.js
+++ front-end/src/components/Account/LoginForm.js
@@ -3,6 +3,8 @@ import {DASHBOARD} from '../../constants/States';
 import RegisterForm from './RegisterForm'
 import {useState} from 'react'
 import './LoginForm.css'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
 function LoginForm({setState, user}) {
     
@@ -52,15 +54,43 @@ function LoginForm({setState, user}) {
                 <div className="input-container">
                     <div className="name-container">
                         <h2>Welcome!</h2>
-                        <label className="label">Username:</label>
-                        <input type='text' placeholder='please enter your user name' value={name} onChange={(e) => {setName(e.target.value)}}/><br/>
+                        {/* <label className="label">Username:</label>
+                        <input type='text' placeholder='please enter your user name' value={name} onChange={(e) => {setName(e.target.value)}}/><br/> */}
+                        <TextField
+                            variant='outlined'
+                            color='primary'
+                            label='Username'
+                            value={name}
+                            onChange={(e) => {setName(e.target.value)}}
+                            style={{
+                                'width': '75%',
+                            }}
+                        /><br/>
                     </div>
+
                     <div className="password-container">
-                        <label className="label">Password: </label>
-                        <input type='password' placeholder='please enter your password' value={password} onChange={(e) => {setPassword(e.target.value)}}/><br/>
+                        {/* <label className="label">Password: </label>
+                        <input type='password' placeholder='please enter your password' value={password} onChange={(e) => {setPassword(e.target.value)}}/><br/> */}
+                        <TextField
+                            variant='outlined'
+                            color='primary'
+                            label='Password'
+                            type='password'
+                            value={password}
+                            onChange={(e) => {setPassword(e.target.value)}}
+                            style={{
+                                'width': '75%',
+                            }}
+                        /><br/>
                     </div>
-                    <button className="sign-in" onClick = {onLogin}> Sign in </button>
-                    <button className="sign-up" onClick = {() => {setHideRgstForm(false)}}> Sign up </button>
+                    {/* <button className="sign-in" onClick = {onLogin}> Sign in </button>
+                    <button className="sign-up" onClick = {() => {setHideRgstForm(false)}}> Sign up </button> */}
+                    <Button variant='contained' color='primary' onClick={onLogin} style={{'width': '30%', 'margin': '5px'}}>
+                        Sign In
+                    </Button>
+                    <Button variant='contained' color='primary'onClick={() => {setHideRgstForm(false)}} style={{'width': '30%', 'margin': '5px'}}>
+                        Sign Up
+                    </Button>
                 </div>
                 <div className="logo-name">
                 <h1>Connect</h1>
