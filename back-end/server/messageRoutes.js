11f948479140bd2cf388ca966afa834536e460e9
Eddy Dinh <eddydinh@Eddys-MacBook-Pro.local>
2021-07-31 00:02:11 -0700

    replace rename file with copyFile

--- back-end/server/messageRoutes.js
+++ back-end/server/messageRoutes.js
@@ -106,7 +106,7 @@ router.post("/addDriftBottlesImages", (req, res) => {
         const uploadImageFile = files.imageFile[0];
         const path_temp = uploadImageFile.path;
         // Move uploaded file with unique name to server public/files folder
-        await fs.rename(path_temp, `${__dirname}/public/driftBottleImages/${hashImageName}`, (err) => {
+        await fs.copyFile(path_temp, `${__dirname}/public/driftBottleImages/${hashImageName}`, (err) => {
             if (err) console.error(err);
         });
         // Save serve URL and file name to database
@@ -149,7 +149,7 @@ router.post("/addTreeHoleThreadImages", (req, res) => {
         const uploadImageFile = files.imageFile[0];
         const path_temp = uploadImageFile.path;
         // Move uploaded file with unique name to server public/files folder
-        await fs.rename(path_temp, `${__dirname}/public/TreeHoleImages/${hashImageName}`, (err) => {
+        await fs.copyFile(path_temp, `${__dirname}/public/TreeHoleImages/${hashImageName}`, (err) => {
             if (err) console.error(err);
         });
         // Save serve URL and file name to database
@@ -191,7 +191,7 @@ router.post("/addDriftBottlesVideos", (req, res) => {
         const uploadVideoFile = files.videoFile[0];
         const path_temp = uploadVideoFile.path;
         // Move uploaded file with unique name to server public/files folder
-        await fs.rename(path_temp, `${__dirname}/public/driftBottleVideos/${hashVideoName}`, (err) => {
+        await fs.copyFile(path_temp, `${__dirname}/public/driftBottleVideos/${hashVideoName}`, (err) => {
             if (err) console.error(err);
         });
         // Save serve URL and file name to database
@@ -233,7 +233,7 @@ router.post("/addTreeHoleThreadVideos", (req, res) => {
         const uploadVideoFile = files.videoFile[0];
         const path_temp = uploadVideoFile.path;
         // Move uploaded file with unique name to server public/files folder
-        await fs.rename(path_temp, `${__dirname}/public/TreeHoleVideos/${hashVideoName}`, (err) => {
+        await fs.copyFile(path_temp, `${__dirname}/public/TreeHoleVideos/${hashVideoName}`, (err) => {
             if (err) console.error(err);
         });
         // Save serve URL and file name to database
@@ -275,7 +275,7 @@ router.post("/addDriftBottlesAudios", (req, res) => {
         const uploadAudioFile = files.audioFile[0];
         const path_temp = uploadAudioFile.path;
         // Move uploaded file with unique name to server public/files folder
-        await fs.rename(path_temp, `${__dirname}/public/driftBottleAudios/${hashAudioName}`, (err) => {
+        await fs.copyFile(path_temp, `${__dirname}/public/driftBottleAudios/${hashAudioName}`, (err) => {
             if (err) console.error(err);
         });
         // Save serve URL and file name to database
@@ -318,7 +318,7 @@ router.post("/addTreeHoleThreadAudios", (req, res) => {
         const uploadAudioFile = files.audioFile[0];
         const path_temp = uploadAudioFile.path;
         // Move uploaded file with unique name to server public/files folder
-        await fs.rename(path_temp, `${__dirname}/public/TreeHoleAudios/${hashAudioName}`, (err) => {
+        await fs.copyFile(path_temp, `${__dirname}/public/TreeHoleAudios/${hashAudioName}`, (err) => {
             if (err) console.error(err);
         });
         // Save serve URL and file name to database
